1276c5cd6265bd87d33b29d2aefb0fdf062d1b88
Jeff Johnson <synotik@hotmail.com>
2020-11-02 07:47:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@ def speak_to_grandma(phrase)
   elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!" 
+    return "I LOVE YOU TOO PUMPKIN!"
   end
 
 end
